f57e4b381e7ef933acd25eb421430bf832102e24
Abeer958 <84776419+Abeer958@users.noreply.github.com>
2022-09-08 12:19:02 +0400

    Added heap

---
+++ heaps/testHeap.js
@@ -0,0 +1,25 @@
+const MinHeap = require('./MinHeap');
+
+// Use this code to test MinHeap class.
+
+// instantiate a MinHeap class
+const minHeap = new MinHeap();
+
+// helper function to return a random integer
+function randomize() { return Math.floor(Math.random() * 40); }
+
+// populate minHeap with random numbers
+for (let i=0; i < 6; i++) {
+  minHeap.add(randomize());
+}
+
+// display the bubbled up numbers in the heap
+console.log('Bubbled Up', minHeap.heap);
+
+// remove the minimum value from heap
+for (let i=0; i < 6; i++) {
+  minHeap.popMin();
+  console.log('Heapified', minHeap.heap);
+}
+
+
